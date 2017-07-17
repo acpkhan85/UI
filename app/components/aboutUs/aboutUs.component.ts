9815a764ff348bf060621d731ce4d718e67b5178
acpkhan85 <acpkhan85@gmail.com>
2017-07-17 17:16:29 +0530

    service call demo

--- app/components/aboutUs/aboutUs.component.ts
+++ app/components/aboutUs/aboutUs.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
- 
+import { AboutUsService } from './aboutussvc.service';
+
 @Component({
    
         moduleId: module.id,
-   templateUrl:'aboutUs.component.html'
-    
+        templateUrl:'aboutUs.component.html',
+        providers :[AboutUsService]
 })
-export class AboutUsComponent { }
+export class AboutUsComponent {        
+
+        constructor(private _aboutUsService  : AboutUsService ){               
+            var result=  _aboutUsService.getAboutUs();                
+                }
+
+
+        ngOnInit():void{
+                this._aboutUsService.getAboutUs();
+        }
+
+}
+
+
+ 
 
 
  
---
+++ app/components/aboutUs/aboutussvc.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+ 
+ @Injectable()
+export class AboutUsService {
+
+    getAboutUs():any
+    {
+
+        return [  {
+        "aboutusImage": ""
+     
+    }];
+    }
+
+ }
+
